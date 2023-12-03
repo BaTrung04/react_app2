b3c9e54b3c37566212ec031c6b9f6e1054eb507d
Ba Trung <batrung0410job@gmail.com>
2023-12-04 01:09:54 +0700

    design question

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,11 +1,17 @@
-import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useParams, useLocation } from 'react-router-dom';
 import { getDataQuiz } from '../../services/apiService';
 import _ from 'lodash';
+import './DetailQuiz.scss'
+import Question from './Question';
 
 const DetailQuiz = (props) => {
     const params = useParams();
+    const location = useLocation();
     const quizId = params.id;
+
+    const [dataQuiz, setDataQuiz] = useState([]);
+    const [index, setIndex] = useState(0);
 
     useEffect(() => {
         fetchQuestions()
@@ -17,9 +23,7 @@ const DetailQuiz = (props) => {
         if (res && res.EC === 0) {
             let raw = res.DT;
             let data = _.chain(raw)
-                // Group the elements of Array based on `color` property
                 .groupBy("id")
-                // `key` is group's name (color), `value` is the array of objects
                 .map((value, key) => {
                     let answers = [];
                     let questionDescription, image = null;
@@ -30,18 +34,56 @@ const DetailQuiz = (props) => {
                             image = item.image;
                         }
                         answers.push(item.answers)
-                        console.log('item answers: ', item.answers)
+                        // console.log('item answers: ', item.answers)
                     })
-                    console.log('value: ', value, 'key: ', key)
+                    //console.log('value: ', value, 'key: ', key)
                     return { questionId: key, answers, questionDescription, image }
                 })
                 .value()
-            console.log("check data", data)
+            console.log(data)
+            setDataQuiz(data)
         }
 
     }
+    console.log('>>>> check data quiz', dataQuiz)
+
+    const handlePrev = () => {
+        if (index - 1 < 0) return;
+        setIndex(index - 1)
+    }
+    const handleNext = () => {
+        if (dataQuiz && dataQuiz.length > index + 1)
+            setIndex(index + 1)
+    }
     return (
-        <div className="detail-quiz-container">sssss</div>
+        <div className="detail-quiz-container container">
+            <div className='left-content'>
+                <div className='title'>
+                    Quiz {quizId}: {location?.state?.quizTitle}
+                </div>
+                <hr />
+                <div className='q-body'>
+                    <img />
+                </div>
+                <div className='q-content'>
+                    <Question
+                        index={index}
+                        data={dataQuiz && dataQuiz.length > 0
+                            ?
+                            dataQuiz[index]
+                            : []
+                        }
+                    />
+                </div>
+                <div className='footer'>
+                    <button className='btn btn-primary ml-3' onClick={() => handlePrev()}>Prev</button>
+                    <button className='btn btn-secondary' onClick={() => handleNext()}>Next</button>
+                </div>
+            </div>
+            <div className='right-content'>
+                count down
+            </div>
+        </div>
     )
 }
 export default DetailQuiz;
