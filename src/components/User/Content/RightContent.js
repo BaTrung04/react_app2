8c86e77668dd1d4c93bb1828f9dc9d29d3ffe1b2
Ba Trung <batrung0410job@gmail.com>
2023-12-07 00:05:34 +0700

    private route

--- src/components/User/Content/RightContent.js
+++ src/components/User/Content/RightContent.js
@@ -24,6 +24,7 @@ const RightContent = (props) => {
     }
 
     const handleClickQuestion = (question, index) => {
+        props.setIndex(index);
         if (refDiv.current) {
             console.log(refDiv.current)
             refDiv.current.forEach(item => {
@@ -39,7 +40,6 @@ const RightContent = (props) => {
             }
         }
         refDiv.current[index].className = "question clicked"
-        props.setIndex(index);
     }
     return (
         <>
